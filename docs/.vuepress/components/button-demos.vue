19ef13baec4ae1867e1969a16384cf8e74500911
zxg <1062661124@qq.com>
2019-04-08 17:38:41 +0800

    使用vuePress基本实现官网的构架

---
+++ docs/.vuepress/components/button-demos.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <g-button>默认按钮</g-button>
+        <g-button :loading=true>默认按钮</g-button>
+        <g-button icon="set">默认按钮</g-button>
+        <g-button disabled>默认按钮</g-button>
+
+        <pre>
+            <code>{{content}}</code>
+        </pre>
+    </div>
+</template>
+<script>
+    import Button from '../../../src/button';
+    export default {
+        components:{
+            "g-button" :Button,
+        },
+        data(){
+            return {
+                content:`
+                    <g-button>默认按钮</g-button>
+                    <g-button :loading=true>默认按钮</g-button>
+                    <g-button icon="set">默认按钮</g-button>
+                    <g-button disabled>默认按钮</g-button>`
+            }
+        }
+    }
+</script>
---
+++ docs/component/button.md
@@ -0,0 +1,9 @@
+---
+title:按钮
+---
+
+# Button
+
+使用方法
+
+<button-demos></button-demos>
